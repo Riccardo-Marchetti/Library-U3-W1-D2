c3b7de9032914e93a90203bfe5a47a775b3fdd4b
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-01-30 23:16:24 +0100

    css

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -2,24 +2,25 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import HorrorBooks from "../Data/horror.json";
 
 const AllBooks = () => {
   return (
     <>
-      <Container className="mt-4">
+      <Container className="pt-4">
         <Row>
           {HorrorBooks.map((book) => {
             return (
               <Col
                 key={book.asin}
-                xs={12}
+                xs={8}
                 sm={6}
                 md={4}
                 lg={3}
                 xl={3}
                 xxl={2}
-                className="mb-4"
+                className="mb-4 ms-auto me-auto"
               >
                 <Card style={{ height: "100%" }}>
                   <Card.Img
@@ -27,11 +28,24 @@ const AllBooks = () => {
                     src={book.img}
                     style={{ height: "300px" }}
                   />
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <Card.Text>category : {book.category}</Card.Text>
-                    <Card.Text>{book.price} €</Card.Text>
-                  </Card.Body>
+                  <Col className="d-flex">
+                    <Card.Body className="d-flex flex-column justify-content-evenly ">
+                      <div>
+                        <Card.Title>{book.title}</Card.Title>
+                      </div>
+                      <div>
+                        <Card.Text className="m-0">
+                          Category : {book.category}
+                        </Card.Text>
+                        <Card.Text>{book.price} €</Card.Text>
+                      </div>
+                      <div className="d-flex mt-3">
+                        <Button className="col-12 bg-black border-white">
+                          Acquista
+                        </Button>
+                      </div>
+                    </Card.Body>
+                  </Col>
                 </Card>
               </Col>
             );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import Alert from "react-bootstrap/Alert";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const WelcomeAllert = () => {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Row className="m-0 pb-2">
+        <Col sm={11} md={9} lg={8} xxl={7} className="  me-auto ms-auto">
+          <Alert
+            className=" border-0  text-center text-black "
+            onClose={() => setShow(false)}
+            dismissible
+          >
+            <Alert.Heading className="fs-4  ">
+              Qua puoi trovare moltissimi libri Horror!
+            </Alert.Heading>
+            <p className="fs-5  ">
+              Registrati per restare sempre aggiornato sulle nuove uscite
+            </p>
+          </Alert>
+        </Col>
+      </Row>
+    );
+  }
   return (
-    <>
-      {["secondary"].map((variant) => (
-        <Container key={"alert"}>
-          <Row className=" mt-2">
-            <Col sm={12} md={8} xl={6} className="me-auto ms-auto">
-              <Alert variant={variant} className="text-center">
-                Benvenuti nel mio negozio di libri Horror!
-              </Alert>
-            </Col>
-          </Row>
-        </Container>
-      ))}
-    </>
+    <Row className="pb-4  ">
+      <Col className="d-flex justify-content-center ">
+        <Button
+          className="bg-black border-white  "
+          onClick={() => setShow(true)}
+        >
+          Mostra di più
+        </Button>
+      </Col>
+    </Row>
   );
 };
 export default WelcomeAllert;
